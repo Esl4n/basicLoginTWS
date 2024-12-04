2a578cba5c1794b3f0f4efa257c1ba114ab9ccf9
dilan.gonzalez <dilan.gonzalez@dgac.gob.cl>
2024-12-04 11:29:30 -0300

    modificando /ping

--- backend/src/controllers/pingController.js
+++ backend/src/controllers/pingController.js
@@ -1,13 +1,5 @@
 const db = require('../models/db')
 
 module.exports.ping = (req,res) =>{
-    const query = 'SELECT * FROM users;'
-
-    try {
-        db.query(query,(err,results)=>{
-            res.json(results)
-        });
-    } catch (e) {
-        console.log(e)
-    }
+    res.send({pong:'pong'})
 }
